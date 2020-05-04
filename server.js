2548457de7fd7174a66cce5fe985852ea110b977
justdheja <justdheja@gmail.com>
2020-05-04 16:17:44 +0700

    pwa

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const server = app.listen(8081, () => {
+    
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('App listen at localhost:%s', port);
+    
+});
